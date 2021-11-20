2b68b5134a2a89069be7fe565c89d1ca4b6d6a0d
jsrathi17 <jayshreerathi111@gmail.com>
2021-11-20 11:51:56 +0200

    Part 1,8 finished

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,5 +1,24 @@
 import React, { useState } from 'react'
 
+const Statistics = ({good, bad, neutral}) =>
+{
+  return (
+
+    <div>
+    <h1>statistics</h1>
+    good {good} <br/>
+    neutral {neutral} <br/>
+    bad {bad} <br/>
+    all {good + neutral + bad}  <br/>
+    average {good/3 + neutral /3 + bad/3} <br/> 
+    positive {`${(good * 100) / (good + neutral + bad)} %`}
+
+    </div>
+
+  )
+
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -13,32 +32,13 @@ const App = () => {
     <button onClick={() => setGood(good + 1)}>good</button>
     <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
     <button onClick={() => setBad(bad + 1)}>bad</button>
-    <h1>statistics</h1>
-    
-    good {good} <br/>
-    neutral {neutral} <br/>
-    bad {bad} <br/>
-    all {good + neutral + bad}  <br/>
-    average {good/3 + neutral /3 + bad/3} <br/> 
-    positive {`${(good * 100) / (good + neutral + bad)} %`}
+    <Statistics good={good} bad={bad} neutral={neutral} />
+
     </div>
     )
 }
 
-// const Display = (props) => {
 
-//   good = props.good
-//   bad = props.bad
-//   neutral = props.neutral
 
-//   return(
-//     <div>
-//       good {good}
-//       neutral {neutral}
-//       bad {bad}
-//     </div>
-//   )
-
-// }
 
 export default App
