18db9a50bc792efa9b3ed8951d2de50511c0a779
jsrathi17 <jayshreerathi111@gmail.com>
2021-11-22 23:25:51 +0200

    Part 2.1-2.5 completed

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -7,9 +7,7 @@ const Course = ({ course }) => {
   return (
     <div>
     <Header name={course.name} />
-    <Part name = {course.parts[0].name} exercises ={course.parts[0].exercises} />
-    <Part name = {course.parts[1].name} exercises ={course.parts[1].exercises} />
-    <Part name = {course.parts[2].name} exercises ={course.parts[2].exercises} />
+    {course.parts.map(part => <Part name={part.name} exercises={part.exercises}/> )}
     <Sum parts = {course.parts} />
 
     </div>
