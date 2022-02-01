b18969798cccae49734a5373a20c6f77796bfbe1
Jayshree Rathi <jayshreerathi111@gmail.com>
2022-02-02 00:26:37 +0200

    Finishing part 4.9 and 4.10

--- part4/bloglist/src/__tests__/blogs_api.test.js
+++ part4/bloglist/src/__tests__/blogs_api.test.js
@@ -26,7 +26,27 @@ test('blogs are returned as json', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body).toHaveLength(BlogsList.listWithMultipleBlogs.length)
   })
+  
+  test('Checking unique identifier property for id', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+    expect(response.body[0]._id).not.toBeDefined()
+})
 
+test('HTTP POST to /api/blogs works', async () => {
+  let response = await api.get('/api/blogs')
+  const initialNumber = response.body.length
+  expect(initialNumber).toBe(BlogsList.listWithMultipleBlogs.length)
+  const newBlog = new Blog({
+      title: "Shark Tank is OKAY",
+      author: "jsrathi",
+      url: "github.com/jsrathi17",
+      likes: 10
+  })
+  await newBlog.save()
+  response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(initialNumber+1)
+})
 
 afterAll(() => {
   mongoose.connection.close()
