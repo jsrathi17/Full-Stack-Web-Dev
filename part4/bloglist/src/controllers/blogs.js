cf2797fb96cad8410c917c0bdc14f573b725c373
Jayshree Rathi <jayshreerathi111@gmail.com>
2022-02-02 00:58:07 +0200

    4.13

--- part4/bloglist/src/controllers/blogs.js
+++ part4/bloglist/src/controllers/blogs.js
@@ -11,7 +11,7 @@ blogRouter.post('/', async (request, response) => {
 
   if (!body.title || !body.url)
   return response.status(400).json({ error: 'title or url is missing' })
-  
+
   let blog = undefined
   if(body.likes === undefined){
     blog = new Blog({
@@ -33,5 +33,31 @@ blogRouter.post('/', async (request, response) => {
   const result = await blog.save()
   response.status(201).json(result)
 })
+
+blogRouter.delete('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if(blog){
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  }
+})
+
+blogRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const blogObject = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  }
+
+  const newBlog = await Blog.findByIdAndUpdate(request.params.id, blogObject, { new: true })
+  response.json(newBlog)
+  response.status(200).end()
+})
+
+
+
+
 
 module.exports = blogRouter
