a92a81f3f89046a43dc439d6b88335e2b5e84832
Jayshree Rathi <jayshreerathi111@gmail.com>
2022-02-02 14:27:05 +0200

    adding 4.15

--- part4/bloglist/src/App.js
+++ part4/bloglist/src/App.js
@@ -6,7 +6,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-
+const usersRouter = require('./controllers/user')
 
 logger.info('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
@@ -19,7 +19,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
-
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
