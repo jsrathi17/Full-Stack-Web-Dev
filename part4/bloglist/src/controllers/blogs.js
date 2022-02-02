6e09826ecb360b2553d6009b306830602f11a4a3
Jayshree Rathi <jayshreerathi111@gmail.com>
2022-02-02 17:10:54 +0200

    adding upto part 4.19

--- part4/bloglist/src/controllers/blogs.js
+++ part4/bloglist/src/controllers/blogs.js
@@ -1,6 +1,17 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blogs')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {username:1, name:1})
   response.json(blogs)
@@ -8,7 +19,12 @@ blogRouter.get('/', async (request, response) => {
 
 blogRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findById(body.userId)
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   if (!body.title || !body.url)
   return response.status(400).json({ error: 'title or url is missing' })
 
