bb54c9b2e060bca96bc7da53f4be751557b2694a
Jayshree Rathi <jayshreerathi111@gmail.com>
2022-02-07 00:31:09 +0200

    part 5.18

--- part5/bloglist-front/cypress/integration/blog_app.spec.js
+++ part5/bloglist-front/cypress/integration/blog_app.spec.js
@@ -1,6 +1,18 @@
 describe('Note app', function() {
+    const user = {
+        "blogs": [],
+        "username": "test",
+        "name": "Jayshree",
+        "password": "test123"
+     }
 
-      it('Login form is shown', function() {
+      beforeEach(function() {
+        /* cy.request('POST', 'http://localhost:3003/api/users',user) */ 
+        cy.visit('http://localhost:3000')
+      })
+
+
+      it('Login form is shown and correct credentials work', function() {
       cy.visit('http://localhost:3000')
       cy.contains('login').click()
       cy.get('#username').type('fullstack')
@@ -8,4 +20,15 @@ describe('Note app', function() {
       cy.get("#login-button").click()
       cy.contains("fullstack logged in as fullstack")
     })
+
+
+    it('Login form is shown and wrong credentials do not work', function() {
+        cy.visit('http://localhost:3000')
+        cy.contains('login').click()
+        cy.get('#username').type('fullstack34')
+        cy.get('#password').type('fullstack34')
+        cy.get("#login-button").click()
+        cy.contains("Wrong credentials!")
+      })
+
   })
